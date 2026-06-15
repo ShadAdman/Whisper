28dd4b79e27c535f4f0701c76e6a26b5720a7de7
shadmanadman <adman.shadman@gmail.com>
2026-06-15 20:26:42 +0100

    Wire AudioEngine into Whisper API

--- whisper/src/commonMain/kotlin/com/whisper/api/Whisper.kt
+++ whisper/src/commonMain/kotlin/com/whisper/api/Whisper.kt
@@ -1,24 +1,40 @@
 package com.whisper.api
 
+import com.whisper.audio.AudioEngine
+import com.whisper.audio.createAudioEngine
 import com.whisper.config.WhisperConfig
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 object Whisper {
+    private val mutex = Mutex()
+    private var engine: AudioEngine? = null
+    private var config: WhisperConfig = WhisperConfig()
 
-    suspend fun startListening() {
-        // TODO: Implement
+    suspend fun startListening() = mutex.withLock {
+        val currentEngine = getOrInitializeEngine()
+        currentEngine.recorder.start()
     }
 
-    suspend fun stopListening() {
-        // TODO: Implement
+    suspend fun stopListening() = mutex.withLock {
+        engine?.recorder?.stop()
     }
 
-    suspend fun transmit(
-        data: ByteArray
-    ) {
-        // TODO: Implement
+    suspend fun transmit(data: ByteArray) = mutex.withLock {
+        val currentEngine = getOrInitializeEngine()
+        // TODO: Use ModemEncoder to convert data to samples
+        val samples = FloatArray(0) 
+        currentEngine.player.play(samples)
     }
     
     fun configure(config: WhisperConfig) {
-        // TODO: Implement
+        this.config = config
+    }
+
+    private suspend fun getOrInitializeEngine(): AudioEngine {
+        return engine ?: createAudioEngine().also {
+            it.setup()
+            engine = it
+        }
     }
 }
