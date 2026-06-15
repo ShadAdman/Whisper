f99948127952711590e035cc382cbcfdb289fe4d
shadmanadman <adman.shadman@gmail.com>
2026-06-15 20:27:28 +0100

    Add listener callback to AudioRecorder

--- whisper-audio/src/desktopMain/kotlin/com/whisper/audio/DesktopAudioRecorder.kt
+++ whisper-audio/src/desktopMain/kotlin/com/whisper/audio/DesktopAudioRecorder.kt
@@ -1,6 +1,11 @@
 package com.whisper.audio
 
 class DesktopAudioRecorder : AudioRecorder {
+    private var listener: ((FloatArray) -> Unit)? = null
+
+    override fun setListener(listener: (FloatArray) -> Unit) {
+        this.listener = listener
+    }
     override suspend fun start() {
         // TODO: Implement using Java Sound API or PortAudio
     }
--- whisper-audio/src/iosMain/kotlin/com/whisper/audio/IOSAudioRecorder.kt
+++ whisper-audio/src/iosMain/kotlin/com/whisper/audio/IOSAudioRecorder.kt
@@ -1,6 +1,11 @@
 package com.whisper.audio
 
 class IOSAudioRecorder : AudioRecorder {
+    private var listener: ((FloatArray) -> Unit)? = null
+
+    override fun setListener(listener: (FloatArray) -> Unit) {
+        this.listener = listener
+    }
     override suspend fun start() {
         // TODO: Implement using AVAudioEngine or AudioUnit
     }
