b4add590bbbc51447c632de1cf3ea6e2f61c3a8d
shadmanadman <adman.shadman@gmail.com>
2026-06-15 11:56:14 +0100

    Add whisper-audio module with platform-specific engine placeholders

---
+++ whisper-audio/src/commonMain/kotlin/com/whisper/audio/AudioEngine.kt
@@ -0,0 +1,9 @@
+package com.whisper.audio
+
+interface AudioEngine {
+    val recorder: AudioRecorder
+    val player: AudioPlayer
+    
+    suspend fun setup()
+    suspend fun release()
+}
---
+++ whisper-audio/src/desktopMain/kotlin/com/whisper/audio/DesktopAudioEngine.kt
@@ -0,0 +1,16 @@
+package com.whisper.audio
+
+class DesktopAudioEngine : AudioEngine {
+    override val recorder: AudioRecorder = object : AudioRecorder {
+        override suspend fun start() { TODO("Not yet implemented") }
+        override suspend fun stop() { TODO("Not yet implemented") }
+    }
+    
+    override val player: AudioPlayer = object : AudioPlayer {
+        override suspend fun play(samples: FloatArray) { TODO("Not yet implemented") }
+        override suspend fun stop() { TODO("Not yet implemented") }
+    }
+
+    override suspend fun setup() { TODO("Not yet implemented") }
+    override suspend fun release() { TODO("Not yet implemented") }
+}
